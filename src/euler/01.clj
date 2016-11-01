ff32f3a10e11c046bfd793eb89b41a4f3f427b65
Scott Zetlan <scott@zetlan.net>
2016-11-01 12:14:46 -0400

    Solution to Euler problem 1.

---
+++ src/euler/01.clj
@@ -0,0 +1,30 @@
+;https://projecteuler.net/problem=1
+
+(ns euler.01
+  (:require [clojure.set :as set]))
+(refer 'euler.util)
+
+(defn multiples-of
+  "Generates a sequence of multiples of N starting at N"
+  [n]
+  (map #(* n %) (iterate inc 1)))
+
+;; Some math makes this simpler.
+;; The highest multiple of 3 that is less than 1,000 is 999. 999 / 3 = 333.
+;; And for 5, it is 995. 995 / 5 = 199.
+;; So we can work with the first 333 multiples of 3 & 995 of 5.
+;;
+;; That gives us two sets, but numbers like 15 or 30 (multiples of 3 AND 5)
+;; appear in both sets. So we need the distinct union of these sets.
+;;
+;; Even more math could simplify further - both sets are simply arithmetic
+;; progressions, and there's a generalized equation for their sums. But this
+;; solution is definitely more LISP-ish that straight-up math.
+(defn euler-1
+  "Sum of the multiples of 3 or 5 below 1,000"
+  []
+  (reduce +
+    (set
+      (set/union
+        (take 333 (multiples-of 3))
+        (take 199 (multiples-of 5))))))
--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,6 +1,7 @@
 (ns euler.core
   (:require
     [euler.util :refer :all]
+    [euler.01 :refer :all]
     [euler.02 :refer :all])
   (:gen-class))
 
@@ -8,6 +9,7 @@
   "Prints answers to solved Euler problems"
   [& args]
 
+  (println "Euler 1: " (euler-1))
   (println "Euler 2: " (euler-2))
 
 
